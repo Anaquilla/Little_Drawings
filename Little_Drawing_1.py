8313a88779a4c85eb45edb1f803ec1b529726eb8
Anaquilla <p.anais74@free.fr>
2026-01-28 21:43:15 +0100

    Add files via upload

---
+++ Little_Drawing_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 28 21:36:11 2026
+
+@author: panai
+"""
+
+import turtle
+
+for i in range(36):
+    turtle.forward(120)
+    turtle.left(170)
+
+turtle.exitonclick()
